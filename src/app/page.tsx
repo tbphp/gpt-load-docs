05bff512c837c92954534efc3eefe03e00c559f6
tbphp <tangb7420@gmail.com>
2025-08-26 11:02:33 +0800

    index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Hero from "../components/Hero";
 import Architecture from "../components/Architecture";
 import QuickStart from "../components/QuickStart";
 import CTA from "../components/CTA";
+import { generatePageMetadata } from "@/lib/dynamicSeo";
+
+// 首页SEO元数据生成
+export async function generateMetadata() {
+  return generatePageMetadata('/');
+}
 
 export default function Home() {
   return (
